3cfa78f067c743df95cc759f36d860803c7c5ffd
hehuazheng <avaforreg@163.com>
2017-05-03 10:42:44 +0800

    add

--- src/main/java/com/hzz/thrift/SimpleThriftClient.java
+++ src/main/java/com/hzz/thrift/SimpleThriftClient.java
@@ -1,19 +1,25 @@
 package com.hzz.thrift;
 
 import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import thrift.api.SimpleParameter;
+import thrift.api.SimpleService;
 
 /**
  * Created by hejf on 2017/3/14.
  */
 public class SimpleThriftClient {
     public static void main(String[] args) throws TException {
-//        TTransport transport = new TSocket("localhost", 9090);
-//        transport.open();
-//        TProtocol protocol = new TBinaryProtocol(transport);
-//        SimpleService.Client client = new SimpleService.Client(protocol);
-//        SimpleParameter sp = new SimpleParameter("a", "b");
-//        String res = client.m1(sp);
-//        System.out.println(res);
-//        transport.close();
+        TTransport transport = new TSocket("localhost", 9090);
+        transport.open();
+        TProtocol protocol = new TBinaryProtocol(transport);
+        SimpleService.Client client = new SimpleService.Client(protocol);
+        SimpleParameter sp = new SimpleParameter("a", "b", "c");
+        String res = client.m1(sp);
+        System.out.println(res);
+        transport.close();
     }
 }
